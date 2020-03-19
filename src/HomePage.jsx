98bfe57d25d5330306a73d558fdcd2bbaf66e71f
Gal Kol <999galk@gmail.com>
2020-03-19 15:53:56 +0200

    base code

---
+++ src/HomePage.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import './HomePage.scss';
+
+const HomePage = () => {
+	return(
+		<div className="homepage">
+			<div className="menu">
+				<div className="menu-item">
+					<div className='content'>
+			          <div className='title'>JACKETS</div>
+			          <span className='subtitle'>SHOP NOW</span>
+			        </div>
+				</div>
+				<div className="menu-item">
+					<div className='content'>
+			          <div className='title'>JACKETS</div>
+			          <span className='subtitle'>SHOP NOW</span>
+			        </div>
+				</div>
+			</div>
+		</div>
+		)
+}
+
+export default HomePage;
