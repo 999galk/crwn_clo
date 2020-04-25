041e51cc69171df8cd361e05c1d63383fabe908b
Gal Kol <999galk@gmail.com>
2020-04-26 00:21:46 +0300

    mid course responsive by myself

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -18,11 +18,10 @@ const Header = ({currentUser, hidden}) => {
 			</Link>
 			<div className='options'>
 				<Link className='option' to='/shop'>SHOP</Link>
-				<Link className='option' to='/contact'>CONTACT</Link>
 				{
 					currentUser ?
 					<div className="option" onClick={() => auth.signOut()}>SIGN OUT</div>
-					: (<Link className='option' to='/signin'>SIGN IN</Link>)
+					: (<Link className='option' to='/signin'>LOGIN</Link>)
 				}
 				<CartIcon/>
 			</div>
